bd91f74b56e8efe704858600e37fd6fc5095d0df
Peter Steinberger <steipete@gmail.com>
2025-08-04 23:03:32 +0200

    fix: Use Tachikoma git URL in PeekabooCore for CI compatibility

--- Core/PeekabooCore/Package.swift
+++ Core/PeekabooCore/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
         .package(url: "https://github.com/modelcontextprotocol/swift-sdk.git", from: "0.9.0"),
         .package(path: "../AXorcist"),
-        .package(path: "../../Tachikoma"),
+        .package(url: "https://github.com/steipete/tachikoma", from: "4.0.0"),
     ],
     targets: [
         .target(
